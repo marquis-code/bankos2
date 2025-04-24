23dec8148fcdaf70ac673d5c3630002fa0574ae7
Marquis <abahmarquis@gmail.com>
2025-04-24 11:46:44 +0100

    updated code

--- components/CsvHandler.vue
+++ components/CsvHandler.vue
@@ -5,13 +5,13 @@
     <div class="flex gap-4 mb-8">
       <button 
         @click="downloadTemplate" 
-        class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 flex items-center"
+        class="bg-blue-600 text-white px-4 py-2 text-sm rounded-full hover:bg-blue-700 flex items-center"
       >
         <span class="mr-2">Download Template</span>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg>
       </button>
       
-      <label class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 cursor-pointer flex items-center">
+      <label class="bg-green-600 text-white text-sm px-4 py-2 rounded-full hover:bg-green-700 cursor-pointer flex items-center">
         <span class="mr-2">Upload CSV/XLSX</span>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-upload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" x2="12" y1="3" y2="15"/></svg>
         <input 
@@ -81,8 +81,10 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import { useUploadFile} from '@/composables/auth/useUploadFile'
 import * as XLSX from 'xlsx'
 import { useFetch } from '#app'
+const { uploadFile, loading: processingupload } = useUploadFile()
 
 interface AccountData {
   [key: string]: string | number
